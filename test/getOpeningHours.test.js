06b20f22b620af148c7bec63e324a0b32bd3411d
Joana <joanamdsantos1@gmail.com>
2022-08-26 21:14:49 -0300

    fazendo os testes da função getOpeningHours, finalizando o requisito 12

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,29 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('1 - Testa se passado nenhum parâmetro, a função retorna um objeto com os horários de atendimento do zoo', () => {
+    const expectedResult = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
+    const calledFunction = getOpeningHours();
+
+    expect(calledFunction).toEqual(expectedResult);
+  });
+
+  it('2 - Espera que a função indique se o zoológico está aberto ou não a partir do dia da semana e do horário passados como parâmetros', () => {
+    const paramsFunction1 = getOpeningHours('Monday', '09:00-AM');
+    const paramsFunction2 = getOpeningHours('Tuesday', '09:00-AM');
+    const paramsFunction3 = getOpeningHours('Wednesday', '09:00-PM');
+    const expectedMessage = 'The zoo is closed';
+    const expectedMessage2 = 'The zoo is open';
+    expect(paramsFunction1).toBe(expectedMessage);
+    expect(paramsFunction2).toBe(expectedMessage2);
+    expect(paramsFunction3).toBe(expectedMessage);
+  });
+});
