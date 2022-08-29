1f9466dd2213d2a26968dd7e149a7e368d97154d
Joana <joanamdsantos1@gmail.com>
2022-08-29 19:52:36 -0300

    quinto requisito finalizado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,18 +1,25 @@
 const data = require('../data/zoo_data');
 
-const { species }   = data;
+const { species } = data;
 
 const countAnimals = (animal) => {
+  if (!animal) {
     return species.reduce((acc, specie) => {
-        const animals = specie.name;
-        if(!acc[animals]) {
-            acc[animals] = specie.residents.length;
-        } 
-        return acc;
+      const animals = specie.name;
+      if (!acc[animals]) {
+        acc[animals] = specie.residents.length;
+      }
+      return acc;
     }, {});
-}
+  }
+  const getSpecies = species.find((specie) => specie.name === animal.specie);
+  const speciesResidents = getSpecies.residents;
+
+  if (!animal.sex) {
+    return speciesResidents.length;
+  }
+  const getSpeciesBySex = speciesResidents.filter((resident) => resident.sex === animal.sex);
+  return getSpeciesBySex.length;
+};
 
 module.exports = countAnimals;
-
-console.log(countAnimals());
-
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,9 +3,8 @@ const data = require('../data/zoo_data');
 const { species } = data;
 
 const getSpeciesByIds = (ids) => {
-  const findId = species.filter((specie) => specie.id === ids);
+  const findId = species.find((specie) => specie.id === ids);
   return findId;
 };
 
 module.exports = getSpeciesByIds;
-
