80eac15a3b364279b9244bea52f2c3ff38423a9c
Joana <joanamdsantos1@gmail.com>
2022-08-30 18:19:05 -0300

    lint corrigido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,8 +2,6 @@ const data = require('../data/zoo_data');
 
 const { species } = data;
 
-
 const getSpeciesByIds = (...ids) => species.filter((specie) => ids.includes(specie.id));
-
 
 module.exports = getSpeciesByIds;
