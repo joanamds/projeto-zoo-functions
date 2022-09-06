09039f52a1a0c68310547898825aa71af0b6775f
Joana <joanamdsantos1@gmail.com>
2022-09-06 12:28:54 -0300

    iniciando as resoluções do requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data; 
+
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+
+
 function isManager(id) {
-  // seu código aqui
+  const getEmployee = employees.find((employee) => employee.id === id);
+  if (getEmployee.managers.length === 0 || getEmployee.managers.length === 1) {
+    return true; 
+  } 
+  return false;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+
 }
 
 module.exports = { isManager, getRelatedEmployees };
