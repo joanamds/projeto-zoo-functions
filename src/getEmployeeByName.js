2c007b46f8d78e152d2a0ee4c2c3c3c7cd78fae9
Joana <joanamdsantos1@gmail.com>
2022-09-06 11:16:09 -0300

    terceiro requisito finalizado

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+
+  const getEmployee = employees.find((employee) => employee.firstName === employeeName
+  || employee.lastName === employeeName);
+  return getEmployee;
 }
 
 module.exports = getEmployeeByName;
