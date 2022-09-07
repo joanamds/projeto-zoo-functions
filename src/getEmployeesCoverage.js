f6aedc4cd1c876cb84348ad3914e5ff2d2c48dfa
Joana <joanamdsantos1@gmail.com>
2022-09-07 19:18:41 -0300

    requisito 11 finalizado

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,22 +2,25 @@ const data = require('../data/zoo_data');
 
 const { species, employees } = data;
 
-function getEmployeesCoverage(employee) {
-  if(!employee) {
-    return employees.reduce((acc, employee) => {
-      const names = employee.firstName;
-      if(!acc[names]){
-        acc[names] = {
-          id: employee.id,
-          fullName: `${employee.firstName} ${employee.lastName}`,
-          species: employee.responsibleFor,
-        }
-      }
-      return acc;
-    }, {});
+function getEmployeesCoverage(entry) {
+  const buildObject = (object) => {
+    const getEmployee = employees.find((employee) => employee.firstName === object.name
+    || employee.lastName === object.name
+    || employee.id === object.id);
+    if (!getEmployee) {
+      throw new Error('Informações inválidas');
+    }
+    const getSpecies = species.filter((specie) => getEmployee.responsibleFor.includes(specie.id));
+    return { id: getEmployee.id,
+      fullName: `${getEmployee.firstName} ${getEmployee.lastName}`,
+      species: getSpecies.map((specie) => specie.name),
+      locations: getSpecies.map((specie) => specie.location),
+    };
+  };
+  if (!entry) {
+    return employees.map((employee) => buildObject(employee));
   }
+  return buildObject(entry);
 }
 
 module.exports = getEmployeesCoverage;
-
-console.log(getEmployeesCoverage());
