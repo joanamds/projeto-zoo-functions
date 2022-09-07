753664205d62b45fe97670b635c80120fe20e6d2
Joana <joanamdsantos1@gmail.com>
2022-09-07 17:26:00 -0300

    décimo requisito finalizado

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const getEmployee = employees.find((employee) => id.includes(employee.id));
+  const responsible = getEmployee.responsibleFor;
+  const getSpecies = species.find((specie) => responsible.includes(specie.id));
+  const animals = getSpecies.residents;
+  const ages = animals.map((resident) => resident.age);
+  const maxAge = Math.max(...ages);
+
+  const oldestAnimal = animals.find((animal) => animal.age === maxAge);
+  const oldestInfo = [`${oldestAnimal.name}`, `${oldestAnimal.sex}`, oldestAnimal.age];
+  return oldestInfo;
 }
 
 module.exports = getOldestFromFirstSpecies;
