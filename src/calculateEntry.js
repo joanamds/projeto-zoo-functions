036a0b7a1c48d1eb902909d11589d6af8654f245
Joana <joanamdsantos1@gmail.com>
2022-09-07 12:03:04 -0300

    iniciando oitavo requisito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,8 +1,9 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
-}
+  }
 
 function calculateEntry(entrants) {
   // seu código aqui
