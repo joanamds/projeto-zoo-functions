5223a8a6c7a92b75bce37b79d66409897f552947
Joana <joanamdsantos1@gmail.com>
2022-09-07 18:03:10 -0300

    iniciando a resolução do requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species, employees } = data;
+
+function getEmployeesCoverage(employee) {
+  if(!employee) {
+    return employees.reduce((acc, employee) => {
+      const names = employee.firstName;
+      if(!acc[names]){
+        acc[names] = {
+          id: employee.id,
+          fullName: `${employee.firstName} ${employee.lastName}`,
+          species: employee.responsibleFor,
+        }
+      }
+      return acc;
+    }, {});
+  }
 }
 
 module.exports = getEmployeesCoverage;
+
+console.log(getEmployeesCoverage());
