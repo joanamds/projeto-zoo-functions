f9352d07eedea1f275b1569be6c6b983c941f94f
Joana <joanamdsantos1@gmail.com>
2022-09-08 10:43:24 -0300

    iniciando a resolução do requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,6 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { species, hours } = data;
+
 function getSchedule(scheduleTarget) {
-}
+    if (!scheduleTarget) {
+        return Object.keys(hours).reduce((acc, hour) => {
+            const weekDay = hour;
+            if (!acc[weekDay]) {
+                acc[weekDay] = {
+                    'officeHour': 'Open from am until pm',
+                    'exhibition': [],
+                };
+            }
+            return acc;
+        }, {});
+    }
+    const speciesFound = species.find((specie) => scheduleTarget === specie.name);
+    return speciesFound.availability;
+};
 
 module.exports = getSchedule;
