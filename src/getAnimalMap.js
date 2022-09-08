ca06e71856833da9724991b510280d8d78afd707
Joana <joanamdsantos1@gmail.com>
2022-09-08 12:48:45 -0300

    iniciando a resolução do exercício 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -3,17 +3,18 @@ const data = require('../data/zoo_data');
 const { species } = data;
 
 function getAnimalMap(options) {
-  if (!options) {
+  if (!options || options.sex) {
     return species.reduce((acc, specie) => {
       const local = specie.location;
       if (!acc[local]) {
         acc[local] = [];
       }
       acc[local].push(specie.name);
-
       return acc;
     }, {});
   }
 }
 
 module.exports = getAnimalMap;
+
+console.log(getAnimalMap());
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -4,18 +4,24 @@ const { species, hours } = data;
 
 function getSchedule(scheduleTarget) {
     if (!scheduleTarget) {
-        return Object.keys(hours).reduce((acc, hour) => {
-            const weekDay = hour;
-            if (!acc[weekDay]) {
-                acc[weekDay] = {
-                    'officeHour': 'Open from am until pm',
+        const weekDays = Object.keys(hours).reduce((acc, hour) => {
+            const days = hour;
+            if (!acc[days]) {
+                acc[days] = {
+                    'officeHours': 'Open from am until pm', 
                     'exhibition': [],
                 };
             }
             return acc;
-        }, {});
+        }, []);
+        return weekDays;
     }
     const speciesFound = species.find((specie) => scheduleTarget === specie.name);
+
+    if(!speciesFound) {
+        const dayFound = hours.find((hour) => scheduleTarget === hour);
+        return dayFound;
+    }
     return speciesFound.availability;
 };
 
