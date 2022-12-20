963e3f0afa69d34a9145eb0ad970c54374d4c42b
Joana <joanamdsantos1@gmail.com>
2022-12-20 13:50:21 -0300

    final commit

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,28 +1,28 @@
-const data = require('../data/zoo_data');
+// const data = require('../data/zoo_data');
 
-const { species, hours } = data;
+// const { species, hours } = data;
 
-function getSchedule(scheduleTarget) {
-    if (!scheduleTarget) {
-        const weekDays = Object.keys(hours).reduce((acc, hour) => {
-            const days = hour;
-            if (!acc[days]) {
-                acc[days] = {
-                    'officeHours': 'Open from am until pm',
-                    'exhibition': [],
-                };
-            }
-            return acc;
-        }, []);
-        return weekDays;
-    }
-    const speciesFound = species.find((specie) => scheduleTarget === specie.name);
+// function getSchedule(scheduleTarget) {
+//     // if (!scheduleTarget) {
+//     //     const weekDays = Object.keys(hours).reduce((acc, hour) => {
+//     //         const days = hour;
+//     //         if (!acc[days]) {
+//     //             acc[days] = {
+//     //                 'officeHours': 'Open from am until pm',
+//     //                 'exhibition': [],
+//     //             };
+//     //         }
+//     //         return acc;
+//     //     }, []);
+//     //     return weekDays;
+//     // }
+//     // const speciesFound = species.find((specie) => scheduleTarget === specie.name);
 
-    if (!speciesFound) {
-        const dayFound = hours.find((hour) => scheduleTarget === hour);
-        return dayFound;
-    }
-    return speciesFound.availability;
-}
+//     // if (!speciesFound) {
+//     //     const dayFound = hours.find((hour) => scheduleTarget === hour);
+//     //     return dayFound;
+//     // }
+//     // return speciesFound.availability;
+// }
 
-module.exports = getSchedule;
+// module.exports = getSchedule;
